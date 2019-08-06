db7ab1ca0d32daca97b586daa2c09cc7d6c16529
陈亮 <1109107755@qq.com>
2019-08-06 15:31:32 +0800

    自适应Web主页

---
+++ js/index.js
@@ -0,0 +1,26 @@
+function on_html5_click() {
+    document.getElementById("id-image-large").setAttribute("src", "images/download.png");
+    document.getElementById("id-image-small").setAttribute("src", "images/html5.png");
+}
+
+function on_css3_click() {
+    document.getElementById("id-image-large").setAttribute("src", "images/js.jpg");
+    document.getElementById("id-image-small").setAttribute("src", "images/js_s.png");
+}
+
+function on_js_click() {
+    document.getElementById("id-image-large").setAttribute("src", "images/css.png");
+    document.getElementById("id-image-small").setAttribute("src", "images/css_s.jpg");
+}
+
+function on_chrome_click() {
+    
+}
+
+function on_firefox_click() {
+    
+}
+
+function on_safari_click() {
+    
+}
